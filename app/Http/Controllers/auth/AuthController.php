85accc5824b39771af9b39d93f061332d1f9f603
Khaiyrus <you@example.com>
2025-05-29 15:43:30 +0700

    menambah fitur register dan login

---
+++ app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class AuthController extends Controller
+{
+    public function formlogin()
+    {
+        return view('auth.login');
+    }
+    public function formregister()
+    {
+        return view('auth.register');
+    }
+    public function register(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role' => 'required|in:panitia,kandidat',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+
+        ]);
+
+        // Redirect atau tampilkan notifikasi
+        return redirect()->route('login')->with('success', 'Pendaftaran berhasil, menunggu persetujuan admin.');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+
+    }
+
+    public function logout(Request $request): RedirectResponse
+{
+    Auth::logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect('/');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('guest');
 });
+Route::get('/login', [AuthController::class, 'formlogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'formregister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/dashboard', [AdminController::class, 'index'])->name('register');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
